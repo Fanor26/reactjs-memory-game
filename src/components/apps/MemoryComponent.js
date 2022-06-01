33d9280aca9f4da074a99f54d82b4a01717b9237
FANOR1945 <aguirrefanor31@gmail.com>
2022-06-01 06:29:11 +0000

    createBoard

--- src/components/apps/MemoryComponent.js
+++ src/components/apps/MemoryComponent.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { createBoard } from "./createBoard";
 import "./styles.css";
 export const MemoryComponent = () => {
+  const list = createBoard(50);
+  const [stateBoard, setStateBoard] = useState(list);
   return (
     <div>
       <h1>ReactJs Memory Game </h1>
       <hr />
       <div className="container">
-        <div className="item">
-          <div className="content">test</div>
-        </div>
+        {stateBoard.map((item) => {
+          return (
+            <div className="item">
+              <div className="content">{item.backend}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/components/apps/createBoard.js
@@ -0,0 +1,34 @@
+export const createBoard = (card) => {
+   if(card %2 !== 0){
+       throw Error("An even number of elements is required")
+
+   }
+   let listBoard= [];
+   let count = 0;
+      for (let i = 0; i < card / 2; i++) {
+        let symbols = String.fromCharCode(70 + count);
+        let item1 = {
+          backend: `??`,
+          frontend: symbols,
+          state: false,
+          done: false,
+          key: `card_${i}`,
+        };
+        let item2 ={ ...item1 };
+        listBoard = [...listBoard, item1, item2];
+        count++;
+      }
+      for (let i = 0; i < listBoard.length; i++) {
+          let index = Math.ceil(Math.random() * listBoard.length -1);
+          let item = listBoard[index];
+          listBoard[index] = listBoard[i];
+          listBoard[i] = item;
+
+      }
+      listBoard = listBoard.map((item, index)=>{
+          item = {id: index, ...item};
+          return item;
+
+      })
+      return listBoard;
+    }

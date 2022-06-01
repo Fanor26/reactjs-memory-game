a457a945ab5be8419b656cc6420fec9f91bb7be5
FANOR1945 <aguirrefanor31@gmail.com>
2022-06-01 05:35:11 +0000

    Create MemoryCompoent

---
+++ src/components/apps/MemoryComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+export const MemoryComponent = () => {
+  return (
+    <div>
+      <h1>ReactJs Memory Game </h1>
+      <hr />
+      <div className="container">
+        <div className="item">
+          <div className="content">test</div>
+        </div>
+      </div>
+    </div>
+  );
+};
